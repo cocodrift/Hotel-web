30b00ba35c6d298ce3351e5a868c52242696b089
mark mwaniki <68171024+cocodrift@users.noreply.github.com>
2024-05-27 22:26:31 +0300

    j

--- utils/currencyConverter.js
+++ utils/currencyConverter.js
@@ -1,23 +1,17 @@
-// utils/currencyConverter.js
 const axios = require('axios');
-require('dotenv').config();
 
 async function convertCurrency(amount, fromCurrency, toCurrency) {
-  const apiKey = process.env.APIKey; // Use environment variable for API key
-  const url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency}`;
+    const apiKey = '8b86775a1db859a105e6cfef';
+    const url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency}`;
 
-  try {
-    const response = await axios.get(url, {
-      headers: {
-        'Authorization': `Bearer ${apiKey}`
-      }
-    });
-    const rate = response.data.rates[toCurrency];
-    return amount * rate;
-  } catch (error) {
-    console.error('Error fetching exchange rates:', error);
-    throw error;
-  }
+    try {
+        const response = await axios.get(url);
+        const rate = response.data.conversion_rates[toCurrency];
+        return amount * rate;
+    } catch (error) {
+        console.error('Error fetching exchange rates:', error);
+        throw error;
+    }
 }
 
 module.exports = convertCurrency;
